428c20733f38c64a9d5db00ced44f5cba7c1edc6
Ori98 <neonpabs@gmail.com>
2023-12-01 15:41:46 -0500

    Added the environment variable for online ATLAS

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ import UserRoutes from "./users/routes.js";
 
 // connecting to mongodb
 import mongoose from 'mongoose';
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas-cs5610-fa23");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas-cs5610-fa23'
+mongoose.connect(CONNECTION_STRING);
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas-cs5610-fa23");
 
 const app = express();
 app.use(express.json());
